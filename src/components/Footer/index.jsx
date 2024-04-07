832f5200cdefa10ed8c7c8c66307151c69c8d410
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-04-07 13:48:29 +0200

    Footer design + making it responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,40 @@ import "./Footer.scss";
 export const Footer = ({ className }) => {
   return (
     <footer className={className}>
-      <div>TOPO</div>
+      <div className="footer__body">
+        <div className="footer__upper">
+          <div className="footer__left">
+            <div className="footer__logo">TOPO</div>
+            <form action="" className="footer__newsletter">
+              <label htmlFor="email">Subscribe to our newsletter!</label>
+              <div>
+                <input type="email" id="email" placeholder="Your Email ..." />
+                <button>Subscribe</button>
+              </div>
+            </form>
+          </div>
+          <div className="footer__right">
+            <div className="footer__list">
+              <div>Socials</div>
+              <p>Facebook</p>
+              <p>Twitter</p>
+              <p>Instagram</p>
+            </div>
+            <div className="footer__list">
+              <div>About Us</div>
+              <div>Contact</div>
+            </div>
+            <div className="footer__list">
+              <div>Career</div>
+              <div>FAQ</div>
+            </div>
+          </div>
+        </div>
+        <div className="footer__lower">
+          <div>Jakob Tidemand</div>
+          <div>&#169;2024</div>
+        </div>
+      </div>
     </footer>
   );
 };
