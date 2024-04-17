96858459dedd68cd295fd74c5d87fb2fc50b6dc9
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-04-17 20:17:57 +0200

    Hero section responsive

---
+++ src/components/Hero/anim.jsx
@@ -0,0 +1,9 @@
+export const cloudPath = {
+  start: { x: "-100%" },
+  end: { x: "100%" },
+  transition: {
+    repeat: Infinity,
+    ease: "linear",
+    duration: 10,
+  },
+};
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,44 @@
+import React from "react";
 import "./Hero.scss";
 import { heroPlanet, cloudOne } from "../../assets/images";
 import { Button } from "../../components/Button";
+import { motion } from "framer-motion";
+import { cloudPath } from "./anim";
 
 export const Hero = () => {
   return (
     <section className="hero__section">
       <img className="hero__image" src={heroPlanet} alt="" />
+      <motion.img
+        initial={{ y: "10%" }}
+        animate={{
+          x: ["-12%", "5%", "-12%"],
+          y: ["-34%"],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 15,
+          ease: "linear",
+        }}
+        src={cloudOne}
+        alt="White 3d cloud"
+        className="cloud__img__small"
+      />
+      <motion.img
+        initial={{ y: "10%" }}
+        animate={{
+          x: ["-12%", "5%", "-12%"],
+          y: [-60],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 15,
+          ease: "linear",
+        }}
+        src={cloudOne}
+        alt="White 3d cloud"
+        className="cloud__img__small2"
+      />
       <h1 className="hero__h1">Booking Made Easy</h1>
       <div className="hero__btn__container">
         <Button to="/venues" className="primary__btn" arrowFillColor="#f9f5f3">
@@ -15,13 +48,28 @@ export const Hero = () => {
           style={{
             "--button-color": "#171717",
             "--button-background": "#f9f5f3",
+            // "--button-background": "transparent",
           }}
           arrowFillColor="#171717"
         >
-          Something
+          Venues
         </Button>
       </div>
-      {/* <img src={cloudOne} alt="" /> */}
+      <motion.img
+        initial={{ y: "10%" }}
+        animate={{
+          x: ["-12%", "-10%", "-12%"],
+          y: [-60],
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 15,
+          ease: "linear",
+        }}
+        src={cloudOne}
+        alt="White 3d cloud"
+        className="cloud__img"
+      />
     </section>
   );
 };
