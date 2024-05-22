e827ff18aae3549a48c5861b7947891153131a2f
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-05-22 11:43:36 +0200

    Started on create venue

--- src/hooks/useProfile.jsx
+++ src/hooks/useProfile.jsx
@@ -1,44 +1,58 @@
 import { useEffect, useState } from "react";
 
-export const useProfile = (token, username) => {
+export const useProfile = () => {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const params = {
+    _bookings: "&_bookings=true",
+    _owner: "?_owner=true",
+    _venues: "?_venues=true",
+    _venue: "?_venue=true",
+    _customer: "&_customer=true",
+  };
 
   useEffect(() => {
-    const fetchProfile = async () => {
-      setLoading(true);
-      setError(null);
+    const userInfo = localStorage.getItem("user");
 
-      try {
-        const response = await fetch(
-          `https://v2.api.noroff.dev/holidaze/profiles/${username}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
+    if (userInfo) {
+      const { name: username, accessToken: token } = JSON.parse(userInfo);
+
+      const fetchProfile = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+          const response = await fetch(
+            `https://v2.api.noroff.dev/holidaze/profiles/${username}/${params._bookings}/${params._venues}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error("Failed to fetch profile data");
           }
-        );
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch profile data");
+          const data = await response.json();
+          setProfile(data.data);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
         }
+      };
 
-        const data = await response.json();
-        setProfile(data.data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (token && username) {
       fetchProfile();
+    } else {
+      setLoading(false);
+      setError("User is not logged in");
     }
-  }, [token, username]);
+  }, []);
 
   return { profile, loading, error };
 };
